4cfafea453edcc3604fff6bdbad0d1108e8984fe
emilio <favorettoemilio@gmail.com>
2025-04-25 09:49:57 -0300

    playlist

---
+++ playlist.js
@@ -0,0 +1,7 @@
+let playlist = [" no regret ", " full moon ", " slaughterhouse", " bloody knuckles "]
+
+let musicaespecifica = playlist.length;
+
+for (let i = 0; i < musicaespecifica; i ++){
+    console.log(playlist[i])
+}
