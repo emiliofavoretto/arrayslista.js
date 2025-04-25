f6b65666cd416e55eefdea99ded1837604e54853
emilio <favorettoemilio@gmail.com>
2025-04-25 11:05:10 -0300

    mostraritem

---
+++ mostraritem.js
@@ -0,0 +1,6 @@
+let itens = [" caderno ", " pedra ", " skate ", " cortina ", " condicionador "];
+
+
+for (let i = itens.lenght ; i >= 0; i--){
+    console.log(itens[i]);
+}
---
+++ mostrarnota.js
@@ -0,0 +1,8 @@
+let notas = [" 10 ", " 8,5 ", " 7 ", " 5 "];
+
+let i = 0;
+
+while(i< notas.length){
+    console.log(" nota " + notas[i]);
+    i++
+}
